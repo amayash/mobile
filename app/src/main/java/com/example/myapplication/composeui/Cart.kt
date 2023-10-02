13712d76730127f1c5cbb703bee15a62ca4c0abb
dasha <amayashimizu89@gmail.com>
2023-10-02 23:54:15 +0400

    купить

--- app/src/main/java/com/example/myapplication/composeui/Cart.kt
+++ app/src/main/java/com/example/myapplication/composeui/Cart.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -20,6 +21,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -138,6 +140,16 @@ fun Cart() {
                 }
             }
         }
+    }
+    Column() {
+        Spacer(modifier = Modifier.weight(1f))
+
+        Button(
+            onClick = { },
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+        ) { Text("Купить") }
     }
 }
 
